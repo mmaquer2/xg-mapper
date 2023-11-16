5ea92a226100c4632972954d7340102e8b63a5ef
Michael Maquera <mike.maquera212@gmail.com>
2023-11-15 19:04:54 -0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,135 @@
-import Image from 'next/image'
+'use client'
+import { useState, useEffect } from 'react';
 import styles from './page.module.css'
 
 export default function Home() {
+
+  const [state,setState] = useState({
+    home_score:0,
+    away_score:0,
+    shots:[],
+    goals:[]
+
+  });
+
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const startClock = () => setIsRunning(true);
+  const stopClock = () => setIsRunning(false);
+
+  // Format time as MM:SS
+  const formatTime = () => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
+
+  
+  const handleFieldClick = (event:any) => {
+    const bounds = event.target.getBoundingClientRect();
+    const x = event.clientX - bounds.left;
+    const y = event.clientY - bounds.top; 
+    
+    console.log("Click Coordinates:", x, y);
+    
+    const shot = {
+      x: x,
+      y: y,
+      time: time
+    }
+
+    console.log(shot);
+    setState(prevState => ({
+      ...prevState,
+      shots: [...prevState.shots, shot]
+    }));
+
+    console.log("shots:", state.shots);
+    submitShot(shot);
+  
+  };
+
+  const submitShot = () => {
+      // todo: submit shot to the database...
+  }
+
+  const exportShotReportToCsv = () => {
+    // export shot report to csv
+    console.log("exporting shots...")
+    state.shots.forEach(shot => {
+      console.log(shot);
+    });
+
+    downloadJson(state.shots, "shots.json");
+
+  }
+
+  function downloadJson(jsonObj:any, fileName:string) {
+    const jsonStr = JSON.stringify(jsonObj);
+    const blob = new Blob([jsonStr], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+  
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  }
+  
+  function recordHomeShot(){
+    // record last x,y selected on the field
+
+  }
+
+  function recordAwayShot(){
+    // record last x,y selected on the field
+
+
+  }
+  
+  
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <p>
+      Welcome to xG Mapper app
+    </p>
+    <p>Game Time: {formatTime()}</p>
+      <button onClick={startClock}>Start</button>
+      <button onClick={stopClock}>Stop</button>
+
+      <button>Record Home Shot</button>
+      <button>Record Away Shot</button>
+
+      <button onClick={() => setTime(0)}>Reset</button>
+    
+      <button onClick = {exportShotReportToCsv}>Generated Shot Report</button>
+
+      <div className={styles.scoreboard}>
+      <p>Home Score</p>
+      <p>away Score</p>
+      <p>shots</p>
+      <p>shots</p>
+
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className={styles.soccerField} onClick={handleFieldClick}>
+      <img src="/field.svg" alt="Soccer Field" />
+    </div>
+  </main>
   )
 }
